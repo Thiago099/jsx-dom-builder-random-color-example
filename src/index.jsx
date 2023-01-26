3b876a74a3deb1e5f8e8bc0f84e8af69c605cd1e
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-26 20:58:50 -0300

    feature

--- src/index.jsx
+++ src/index.jsx
@@ -17,6 +17,7 @@ generate_random_colors()
 
 const app = <div class="main-item" draggable>Click on me to change the color</div>
 
+// you can edit the element after its creation on the jsx, that allows a lot of extra interactions that you cant do without it
 app
 .$style('background-color', background)
 .$style('color', foreground)
@@ -24,6 +25,8 @@ app
     const [random_color,inverse] = generate_random_colors()
     foreground = random_color
     background = inverse
+    // the update function does manually what the effect does automatically, after calling the update function
+    // the dynamic part of the element and its children will be updated
     app.$update()
 })
 
